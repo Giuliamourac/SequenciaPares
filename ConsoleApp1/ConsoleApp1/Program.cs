f2c2462ac746d23e53d8f5e12082eafa8c0e3090
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-02-27 11:54:12 -0300

    finaliza programa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 0;
+
+        Console.WriteLine("Digite um número para a listagem de todos dos pares entre este e 0. ");
+
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Os pares são:");
+
+        for (int i = 0; i <= numero; i++)
+        {
+
+            if (i % 2 == 0)
+            {
+
+                Console.WriteLine($"{i}");
+
+            }
+
+            else
+            {
+
+
+            }
+
+        }
+
+    }
+
+}
